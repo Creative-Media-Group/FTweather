38caf8afbfa0d6c60f302011c796a65940f0eaf3
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-27 23:34:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ import myweather as mr
 
 def main(page: ft.Page):
     page.title = "FTweather"
+    page.theme = ft.Theme(color_scheme_seed="red")
+    page.update()
     page.appbar = ft.AppBar(title=ft.Text(page.title))
     dv.load_dotenv()
     API_KEY = os.getenv("OPENWEATHER_DEV_API")
     weather = mr.MyWeather(api_key=API_KEY, lat=34, lon=34, setgraddegree="Grad")
     page.add(
         ft.SafeArea(
-            ft.Text(weather.degreefunc()),
+            ft.Column(
+                controls=[ft.Text(weather.degreefunc()), ft.Text(weather.description())]
+            ),
         )
     )
-    page.add(ft.SafeArea(ft.Text(weather.description())))
 
 
 ft.app(main)
