358d40a0336842133dbeac70bc99886c8148325a
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-10 19:24:56 +0100

    .

--- main.py
+++ main.py
@@ -7,34 +7,35 @@ dv.load_dotenv()
 API_KEY = os.getenv("OPENWEATHER_DEV_API")
 weather = mr.MyWeather(api_key=API_KEY, lat=34, lon=34, setgraddegree="Grad")
 
-def weatherdialog(page, city, description):
-    dlg = ft.AlertDialog(open=True)
-
-def city(page, city, key=API_KEY):
-    container = ft.Container(
-        content=ft.Column(
-            controls=[
-                ft.Icon(name=ft.icons.CLOUD),
-                ft.Text(value=weather.degreefunc()),
-                ft.Text(city)
-            ],
-            alignment="center",
-            spacing=6
-        ),
-        alignment=ft.alignment.center,
-        border_radius=10,
-        width=80,
-        height=90,
-        bgcolor=ft.colors.AMBER,
-        on_click=lambda _ : weatherdialog(page=page)
-    )
-    return container
-
 
 def main(page: ft.Page):
+    def i(e):
+        print(e)
+
+    def city(city, key=API_KEY):
+        container = ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.Icon(name=ft.icons.CLOUD),
+                    ft.Text(value=weather.degreefunc()),
+                    ft.Text(city),
+                ],
+                alignment="center",
+                spacing=6,
+            ),
+            alignment=ft.alignment.center,
+            border_radius=10,
+            width=80,
+            height=90,
+            bgcolor=ft.colors.AMBER,
+            on_click=lambda e: i(e=e),
+        )
+
+        return container
+
     page.title = "FTweather"
     page.update()
-    page.appbar = ft.AppBar(title=ft.Text(page.title),center_title=True)
+    page.appbar = ft.AppBar(title=ft.Text(page.title), center_title=True)
 
     page.add(
         ft.SafeArea(
