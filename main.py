0d967b3126bc9965eb87e292d223fb97cc2c59fd
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-10 17:03:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import flet as ft
 import os
 import dotenv as dv
 import myweather as mr
+dv.load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_DEV_API")
 
+def city(page, key=API_KEY, city):
+    weather = mr.MyWeather(api_key=API_KEY, lat=34, lon=34, setgraddegree="Grad")
 
 def main(page: ft.Page):
     page.title = "FTweather"
     page.theme = ft.Theme(color_scheme_seed="white")
     page.update()
     page.appbar = ft.AppBar(title=ft.Text(page.title))
-    dv.load_dotenv()
-    API_KEY = os.getenv("OPENWEATHER_DEV_API")
-    weather = mr.MyWeather(api_key=API_KEY, lat=34, lon=34, setgraddegree="Grad")
+    
     page.add(
         ft.SafeArea(
             ft.Column(
