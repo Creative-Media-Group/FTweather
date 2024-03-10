6914584e5da353bad1c95c8794ef7b9c0b751fbc
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-10 18:16:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,28 @@ API_KEY = os.getenv("OPENWEATHER_DEV_API")
 
 def city(page, city, key=API_KEY):
     weather = mr.MyWeather(api_key=API_KEY, lat=34, lon=34, setgraddegree="Grad")
-    container = ft.Container(content=ft.Column(controls=[ft.Icon(name=ft.icons.CLOUD),ft.Text()]),border_radius=10,width=80, height=80,bgcolor=ft.colors.AMBER)
+    container = ft.Container(
+        content=ft.Column(
+            controls=[
+                ft.Icon(name=ft.icons.CLOUD),
+                ft.Text(value=weather.degreefunc()),
+            ],
+            alignment="center",
+            spacing=10
+        ),
+        alignment=ft.alignment.center,
+        border_radius=10,
+        width=80,
+        height=80,
+        bgcolor=ft.colors.AMBER,
+    )
     return container
 
 
 def main(page: ft.Page):
     page.title = "FTweather"
     page.update()
-    page.appbar = ft.AppBar(title=ft.Text(page.title))
+    page.appbar = ft.AppBar(title=ft.Text(page.title),center_title=True)
 
     page.add(
         ft.SafeArea(
